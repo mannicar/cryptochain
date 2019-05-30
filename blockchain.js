e55af8578a9b0ef909225f26a4180a4da3ba5ef2
Carlo Mannino <manninosca@gmail.com>
2019-05-30 14:14:22 -0400

    replacechain

--- blockchain.js
+++ blockchain.js
@@ -16,15 +16,28 @@ class Blockchain {
         this.chain.push(newBlock);
     };
 
+    replaceChain(chain){
+        if (chain.length <= this.chain.length) {
+            console.error('Incoming chain must be longer');
+            return;
+        }
+
+        if (!Blockchain.isValidChain(chain)) {
+            console.error('Incoming chain must be valid');
+            return;
+        }
+
+        console.log('Replacing chain with ', chain);
+        this.chain = chain;
+    };
+
 
     static isValidChain(chain) {
         if (JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) { return false; }
 
         for (let i = 1; i < chain.length; i++) {
             const block = chain[i];
-
             const actualLastHash = chain[i-1].hash;
-
             const {timestamp, lastHash, hash, data} = block;
 
             if (lastHash !== actualLastHash) return false;
