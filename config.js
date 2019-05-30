7331394c34e908d97fa0ac65c1c68166f40eb7b8
Carlo Mannino <manninosca@gmail.com>
2019-05-30 16:33:55 -0400

    Nonce, difficulty

--- config.js
+++ config.js
@@ -1,7 +1,11 @@
+const INITIAL_DIFFICULTY = 5;
+
 const GENESIS_DATA = {
     timestamp:1,
     lastHash:'-----',
     hash:'hash-one',
+    difficulty: INITIAL_DIFFICULTY,
+    nonce: 0,
     data: []
 };
 
