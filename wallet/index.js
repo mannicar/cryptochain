13a0a2b62ff719bdf819eb1a96433634028dc940
Carlo Mannino <manninosca@gmail.com>
2019-05-31 15:22:38 -0400

    verify signatures

--- wallet/index.js
+++ wallet/index.js
@@ -1,13 +1,17 @@
 const { STARTING_BALANCE } = require('../config');
 const { ec } = require('../util');
+const cryptoHash = require('../util/crypto-hash');
 
 class Wallet {
 
     constructor() {
-        const keyPair = ec.genKeyPair();
-
         this.balance = STARTING_BALANCE;
-        this.publicKey = keyPair.getPublic().encode('hex');
+        this.keyPair = ec.genKeyPair();
+        this.publicKey = this.keyPair.getPublic().encode('hex');
+    };
+
+    sign(data) {
+        return this.keyPair.sign(cryptoHash(data));
     };
 
 }
