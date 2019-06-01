0cbc3bb4681b2812991fba52645f5e396a71ce1a
Carlo Mannino <manninosca@gmail.com>
2019-05-31 20:18:22 -0400

    transact endpoint pt1

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const request = require('request');
 const Blockchain = require('./blockchain');
 const bodyParser = require('body-parser');
 const PubSub = require('./app/pubsub');
+const TransactionPool = require('./wallet/transaction-pool');
+const Wallet = require('./wallet')
 const {DEFAULT_PORT, HOSTNAME} = require('./config');
 
 const app = express();
 const blockchain = new Blockchain();
+const transactionPool = new TransactionPool();
+const wallet = new Wallet();
 const pubsub = new PubSub({blockchain});
 
 const ROOT_NODE_ADDRESS = `http://${HOSTNAME}:${DEFAULT_PORT}`
@@ -25,6 +29,17 @@ app.post('/api/mine', (req, res) => {
 
     pubsub.broadcastChain();
     res.redirect('/api/blocks');
+});
+
+app.post('/api/transact', (req, res) => {
+    const { amount, recipient } = req.body;
+    const transaction = wallet.createTransaction({ recipient, amount });
+
+    transactionPool.setTransaction(transaction);
+
+    console.log('transactionPool', transactionPool);
+
+    res.json({ transaction });
 });
 
 // Initialize server
