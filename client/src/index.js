8cbc11d2a40ce9d4b822cca5081d6aee5a3eaa35
Carlo Mannino <manninosca@gmail.com>
2019-06-01 23:04:04 -0400

    Transaction Pool fucntionality

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-dom';
 import App from './components/App';
 import Blocks from './components/Blocks';
 import ConductTransaction from './components/ConductTransaction';
+import TransactionPool from './components/TransactionPool';
 import './index.css';
 import { Router, Switch, Route} from 'react-router-dom';
 import history from './history';
@@ -15,6 +16,7 @@ render(
             <Route exact path='/'component={App} />
             <Route path='/blocks'component={Blocks} />
             <Route path='/conduct-transaction' component={ConductTransaction}/>
+            <Route path='/transaction-pool' component={TransactionPool}/>
         </Switch>
     </Router> ,
     document.getElementById('root')
