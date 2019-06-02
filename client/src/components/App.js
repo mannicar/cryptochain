5e6c36ae62cfd45a17e43769ada9f7d7a6052a44
Carlo Mannino <manninosca@gmail.com>
2019-06-01 20:38:45 -0400

    Blocks component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import Blocks from './Blocks';
 
 class App extends Component {
+    state = { walletInfo: {}}; // class property syntax of Javascript
+
+    componentDidMount() {
+        fetch('http://localhost:3000/api/wallet-info')
+            .then(response => response.json())
+                .then(json => this.setState({ walletInfo:json }));
+    }
+
     render() {
+        const {address, balance} = this.state.walletInfo;
         return (
             <div>
-                Welcome to the Blockchain!!! :D
+                <div>Welcome to the Blockchain... .. ..  .  .   .</div>
+                <div>Address: {address}</div>
+                <div>Balance: {balance}</div>
+                <br />
+                <Blocks />
             </div>
         );
     };
